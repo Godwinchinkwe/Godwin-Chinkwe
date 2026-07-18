0412e419be9768ca2210b5633b9ca5bc92470889
Godwin Chinkwe <chinkwegodwin@gmail.com>
2026-07-18 06:48:55 +0100

    first commit

---
+++ src/lib/constants.ts
@@ -0,0 +1,14 @@
+export const COLORS = {
+  background: "#050505",
+  surface: "#101010",
+  card: "#181818",
+
+  primary: "#805AA3",
+  accent: "#A98024",
+
+  white: "#FFFFFF",
+  gray: "#9CA3AF",
+
+  success: "#10B981",
+  danger: "#EF4444",
+};
