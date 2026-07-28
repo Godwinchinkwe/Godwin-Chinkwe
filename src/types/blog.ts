7ea1eee0d2df067d9f4ba83f892f0707367e3eef
Godwin Chinkwe <chinkwegodwin@gmail.com>
2026-07-28 08:04:01 +0100

    reveal

--- src/types/blog.ts
+++ src/types/blog.ts
@@ -1,31 +1,20 @@
 export type BlogPost = {
+
   slug: string;
-
   title: string;
-
   excerpt: string;
-
   content: string;
-
   category: string;
-
   author: string;
-
   publishedAt: string;
-
   readingTime: string;
-
   image: string;
-
   featured?: boolean;
-
   tags: string[];
-
   sections: BlogSection[];
 };
 
 export type BlogSection = {
   heading: string;
-
   paragraphs: string[];
 };
